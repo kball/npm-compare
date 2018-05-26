0137a2fef18475470516961173c5377209e1de7a
Kevin Ball <kmball11@gmail.com>
2018-05-26 08:42:17 -0700

    Add rate limiting to api use

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import { Package, PackageDownloads } from './models';
+import { RateLimiter } from 'limiter';
 const RegClient = require('npm-registry-client');
 
 const client = new RegClient();
 const uri = `https://registry.npmjs.org/`;
 const params = {timeout: 1000};
+const limiter = new RateLimiter(1, 1000);
 
+function getLimited(uri, params, cb) {
+  limiter.removeTokens(1, () => {
+    client.get(uri, params, cb);
+  });
+}
 
 function packageCreated(pkgName, cb) {
-  client.get(`${uri}${pkgName}`, {}, (err, data) => {
+  getLimited(`${uri}${pkgName}`, {}, (err, data) => {
     if(err) {
       console.log('err', err);
     }
-    if(data.time) {
+    if(data && data.time) {
       cb(data.time.created)
     }
   });
@@ -30,7 +37,7 @@ function savePackage(obj, tag) {
 
 function addVuePackages(offset = 0) {
   const url = `${uri}-/v1/search?text=vue&from=${offset}`;
-  client.get(url, {}, (err, data, raw, res) => {
+  getLimited(url, {}, (err, data, raw, res) => {
     data.objects.forEach((obj) => {
       savePackage(obj.package, 'vue');
     });
